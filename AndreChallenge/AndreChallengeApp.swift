182770bf68ce7c0222c507efdcec390d6c3ce85f
shanezzar <shanezzar@hotmail.com>
2021-11-24 11:34:21 +0100

    Initial Commit

---
+++ AndreChallenge/AndreChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AndreChallengeApp.swift
+//  AndreChallenge
+//
+//  Created by Shanezzar Sharon on 24/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct AndreChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AndreChallenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AndreChallenge
+//
+//  Created by Shanezzar Sharon on 24/11/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
